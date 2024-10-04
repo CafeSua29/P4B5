42afb8278cf89efd1c2d31d2b6ba2cf616ad351a
CafeSua29 <luongbaphananh@gmail.com>
2024-10-04 15:36:28 +0700

    7

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -8,34 +8,34 @@ object Main {
   val spark = SparkSession.builder().getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
 
-  private def createReplaceTable(connection: Connection, inpTableName: String): Unit = {
-    val admin = connection.getAdmin
-    val tableName = TableName.valueOf(inpTableName)
-
-    if (admin.tableExists(tableName)) {
-      println(s"Table $tableName exists. Deleting...")
-      admin.disableTable(tableName)
-      admin.deleteTable(tableName)
-    }
-
-    if (!admin.tableExists(tableName)) {
-      val tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(tableName)
-
-      val departmentFamily = ColumnFamilyDescriptorBuilder.newBuilder("department".getBytes).build()
-      val salaryFamily = ColumnFamilyDescriptorBuilder.newBuilder("salary".getBytes).build()
-      val titleFamily = ColumnFamilyDescriptorBuilder.newBuilder("title".getBytes).build()
-
-      tableDescriptorBuilder.setColumnFamily(departmentFamily)
-      tableDescriptorBuilder.setColumnFamily(salaryFamily)
-      tableDescriptorBuilder.setColumnFamily(titleFamily)
-
-      admin.createTable(tableDescriptorBuilder.build())
-      println("Bảng đã được tạo thành công!")
-    } else {
-      println("Bảng đã tồn tại.")
-    }
-    admin.close()
-  }
+//  private def createReplaceTable(connection: Connection, inpTableName: String): Unit = {
+//    val admin = connection.getAdmin
+//    val tableName = TableName.valueOf(inpTableName)
+//
+//    if (admin.tableExists(tableName)) {
+//      println(s"Table $tableName exists. Deleting...")
+//      admin.disableTable(tableName)
+//      admin.deleteTable(tableName)
+//    }
+//
+//    if (!admin.tableExists(tableName)) {
+//      val tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(tableName)
+//
+//      val departmentFamily = ColumnFamilyDescriptorBuilder.newBuilder("department".getBytes).build()
+//      val salaryFamily = ColumnFamilyDescriptorBuilder.newBuilder("salary".getBytes).build()
+//      val titleFamily = ColumnFamilyDescriptorBuilder.newBuilder("title".getBytes).build()
+//
+//      tableDescriptorBuilder.setColumnFamily(departmentFamily)
+//      tableDescriptorBuilder.setColumnFamily(salaryFamily)
+//      tableDescriptorBuilder.setColumnFamily(titleFamily)
+//
+//      admin.createTable(tableDescriptorBuilder.build())
+//      println("Bảng đã được tạo thành công!")
+//    } else {
+//      println("Bảng đã tồn tại.")
+//    }
+//    admin.close()
+//  }
 
   private def createDataFrameAndPutToHBase(): Unit = {
     val jdbcUrl = "jdbc:mysql://localhost:3306/employees"
@@ -50,9 +50,9 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    val hbaseConnection = HBaseConnectionFactory.createConnection()
-    val tableName = "employees"
-    createReplaceTable(hbaseConnection, tableName)
+//    val hbaseConnection = HBaseConnectionFactory.createConnection()
+//    val tableName = "employees"
+//    createReplaceTable(hbaseConnection, tableName)
     createDataFrameAndPutToHBase()
   }
 }
