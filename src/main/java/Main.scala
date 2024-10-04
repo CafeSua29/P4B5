ea73cf17b6fa6333d30cf668eaea246bcd8b1fe4
CafeSua29 <luongbaphananh@gmail.com>
2024-10-04 14:06:21 +0700

    1

---
+++ src/main/java/Main.scala
@@ -0,0 +1,50 @@
+import org.apache.hadoop.hbase.client.{ColumnFamilyDescriptorBuilder, Connection, TableDescriptorBuilder}
+import org.apache.hadoop.hbase.{NamespaceDescriptor, TableName}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType, TimestampType}
+
+object Main {
+  val spark = SparkSession.builder().getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  private def createReplaceTable(connection: Connection, inpTableName: String): Unit = {
+    val admin = connection.getAdmin
+    val tableName = TableName.valueOf("employees")
+
+    if (admin.tableExists(tableName)) {
+      println(s"Table $tableName exists. Deleting...")
+      admin.disableTable(tableName)
+      admin.deleteTable(tableName)
+    }
+
+    if (!admin.tableExists(tableName)) {
+      val tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(tableName)
+
+      val departmentFamily = ColumnFamilyDescriptorBuilder.newBuilder("department".getBytes).build()
+      val salaryFamily = ColumnFamilyDescriptorBuilder.newBuilder("salary".getBytes).build()
+      val titleFamily = ColumnFamilyDescriptorBuilder.newBuilder("title".getBytes).build()
+
+      tableDescriptorBuilder.setColumnFamily(departmentFamily)
+      tableDescriptorBuilder.setColumnFamily(salaryFamily)
+      tableDescriptorBuilder.setColumnFamily(titleFamily)
+
+      admin.createTable(tableDescriptorBuilder.build())
+      println("Bảng đã được tạo thành công!")
+    } else {
+      println("Bảng đã tồn tại.")
+    }
+    admin.close()
+  }
+
+  private def createDataFrameAndPutToHDFS(): Unit = {
+    val jdbcUrl = "jdbc:mysql://localhost:3306/employees"
+    val jdbcProperties = new java.util.Properties()
+    jdbcProperties.setProperty("user", "root")
+    jdbcProperties.setProperty("password", "123456")
+
+    val employeesDF = spark.read
+      .jdbc(jdbcUrl, "employees", jdbcProperties)
+
+    employeesDF.show()
+  }
+}
